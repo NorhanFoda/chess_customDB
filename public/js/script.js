96d09ba832c7e8bf85ad1b57be14d49c299dea20
NorhanFoda <norhan.h.foda@gmail.com>
2019-11-20 15:39:13 +0200

    20-11-2019-3:39pm

--- public/js/script.js
+++ public/js/script.js
@@ -2,30 +2,30 @@ var board,
     game = new Chess();
 
     //evaluate action_library
-    var eval_arr="";
-    $.ajax({
-        url: "http://127.0.0.1:8000/allReacions",
-        type: "GET",
-        dataType: "json",
-        success: function(data){
-            for(var i = 0; i < data.length; i++){
-                game.load(data[i].black_reaction_fen);
-                eval = -evaluateBoard(game.board());
-                eval_arr += eval+',';
-            }
-            // console.log(eval_arr);
-            var id = 1;
-            $.ajax({
-                url: "http://127.0.0.1:8000/setEval",
-                type: "POST",
-                dataType: "json",
-                data: {eval_arr:eval_arr},
-                success: function(data2){
-                    console.log(data2);
-                }
-            });
-        }
-    });
+    // var eval_arr="";
+    // $.ajax({
+    //     url: "http://127.0.0.1:8000/allReacions",
+    //     type: "GET",
+    //     dataType: "json",
+    //     success: function(data){
+    //         for(var i = 0; i < data.length; i++){
+    //             game.load(data[i].black_reaction_fen);
+    //             eval = -evaluateBoard(game.board());
+    //             eval_arr += eval+',';
+    //         }
+    //         // console.log(eval_arr);
+    //         var id = 1;
+    //         $.ajax({
+    //             url: "http://127.0.0.1:8000/setEval",
+    //             type: "POST",
+    //             dataType: "json",
+    //             data: {eval_arr:eval_arr},
+    //             success: function(data2){
+    //                 console.log(data2);
+    //             }
+    //         });
+    //     }
+    // });
 
 /*The "AI" part starts here */
 var minimaxRoot = function(depth, game, isMaximisingPlayer, white_move) {
